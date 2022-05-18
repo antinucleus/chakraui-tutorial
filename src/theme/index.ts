a8db75971ff9bf6a72aa0416d58b972f0b3a8c7a
antinucleus <antinucleuss@gmail.com>
2022-05-18 17:11:05 +0300

    Override the Built-in Component's Styles in Chakra UI

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,6 +5,24 @@ import {
   withDefaultVariant,
 } from '@chakra-ui/react';
 
+const inputSelectStyles = {
+  variants: {
+    filled: {
+      field: {
+        _focus: {
+          borderColor: 'brand.500',
+        },
+      },
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        borderRadius: 'none',
+      },
+    },
+  },
+};
 const theme = extendTheme(
   {
     colors: {
@@ -24,6 +42,21 @@ const theme = extendTheme(
     fonts: {
       heading: `Montserrat ${base.fonts?.heading}`,
       body: `Inter ${base.fonts?.body}`,
+    },
+    components: {
+      Input: { ...inputSelectStyles },
+      Select: { ...inputSelectStyles },
+      Checkbox: {
+        baseStyle: {
+          control: {
+            borderRadius: 'none',
+            _focus: {
+              ring: 2,
+              ringColor: 'brand.500',
+            },
+          },
+        },
+      },
     },
   },
   withDefaultColorScheme({
