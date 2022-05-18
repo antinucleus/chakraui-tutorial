8d3f5bf509629d62702c90aaaeb256ca03974cb5
antinucleus <antinucleuss@gmail.com>
2022-05-18 15:50:12 +0300

    Build a 2-Column Form with the SimpleGrid, FormControl, and Input Component in Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Container, Flex, VStack } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
+
+import Cart from './sections/cart';
+import Details from './sections/details';
 import './App.css';
 
 function App() {
   return (
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" />
-        <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50" />
+        <Details />
+        <Cart />
       </Flex>
     </Container>
   );
---
+++ src/sections/cart.tsx
@@ -0,0 +1,7 @@
+import { VStack } from '@chakra-ui/react';
+
+const Cart = () => {
+  return <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50" />;
+};
+
+export default Cart;
