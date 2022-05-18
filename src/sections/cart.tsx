0d5d0fd60ed7c061c699a668cb0e47f6f41ec31c
antinucleus <antinucleuss@gmail.com>
2022-05-18 16:19:29 +0300

    Create a Dark Mode Switcher in Chakra UI

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -1,7 +1,77 @@
-import { VStack } from '@chakra-ui/react';
+import {
+  VStack,
+  HStack,
+  Heading,
+  Text,
+  Button,
+  AspectRatio,
+  Image,
+  Stack,
+  Divider,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 const Cart = () => {
-  return <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg="gray.50" />;
+  const { toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50');
+  const secondaryTextColor = useColorModeValue('gray.600', 'gray.400');
+
+  return (
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="flex-start" bg={bgColor}>
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading>Your cart</Heading>
+        <Text>
+          If the prices is too hard on your eyes,
+          <Button onClick={toggleColorMode} variant="unstyled" colorScheme="blackAlpha">
+            try changing the theme.
+          </Button>
+        </Text>
+      </VStack>
+      <HStack spacing={6} alignItems="center" w="full">
+        <AspectRatio ratio={1} w={24}>
+          <Image
+            alt="linkedin"
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQHaL6JYA9zDrQ/profile-displayphoto-shrink_800_800/0/1640042501433?e=1658361600&v=beta&t=vB8suv_7_HKqDN2aC7mnBEuZFUBr9cGapw57CLbt5IE"
+          />
+        </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="flex-start">
+            <Heading size="md">Penny Board</Heading>
+            <Text color={secondaryTextColor}>PNYCOMP27541</Text>
+          </VStack>
+          <Heading size="md" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
+      </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Subtotal</Text>
+          <Heading size="sm">$119.00</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Shipping</Text>
+          <Heading size="sm">$19.99</Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Taxes (estimated)</Text>
+          <Heading size="sm">$23.80</Heading>
+        </HStack>
+      </VStack>
+      <Divider />
+      <HStack justifyContent="space-between" w="full">
+        <Text color={secondaryTextColor}>Total</Text>
+        <Heading size="lg">$162.79</Heading>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Cart;
